d2a79effaa49f1383e2079efd298d3b2a44fdb3b
Narayan Msra <arayannmsra@MacBook-Pro.local>
2025-01-24 22:04:39 -0500

    blocks, scripts, index.html

--- scripts/index.js
+++ scripts/index.js
@@ -30,4 +30,50 @@ const initialCards = [
   },
 ];
 
-console.log(initialCards);
+//*----------------ELEMENT---------*/
+const profileEditButton = document.querySelector("#profile__editButton");
+const profileTitle = document.querySelector(".profile__title");
+const profile__addButton = document.querySelector("#profile__add-CardButton");
+const profileBio = document.querySelector(".profile__bio");
+const cardList = document.querySelector(".cards__list");
+const closeButtons = document.querySelector(".modal__close");
+
+//----------------Modal section-----------/
+const profileEditModal = document.querySelector("#modal-edit-profile");
+const profileEdit_name = document.querySelector["profile__EditModal"];
+const cardAdd = document.querySelector["add__cardModal"];
+const profileNameInput = document.querySelector("#profile-NameModal");
+const profileBioInput = document.querySelector("#profile-bioModal");
+const modalAddCard = document.querySelector("#modal-add-card");
+const addCardModalForm = document.querySelector["add__cardModal"];
+const modalInputForm = document.querySelector("#modal-InputForm");
+const modalFormLink = document.querySelector("#Input-LinkModal");
+const modalImagePreview = document.querySelector("#modal-image-preview");
+const modalImageCaption = document.querySelector(".modal__image-caption");
+
+profileEditButton.addEventListener("click", () => {
+  profileEditModal.classList.add("modal_opened");
+});
+
+function closeModal(modal) {
+  modal.classList.remove("modal_opened");
+}
+
+closeButtons.addEventListener("click", () => {
+  console.log("clicked");
+  closeModal(profileEditModal);
+});
+
+// /*
+// profileEditButton.onclick = () =>
+//   profileEditModal.classList.add("modal_opened");
+
+// closeButtons.onclick = () => {
+//   console.log("clicked");
+//   profileEditModal.classList.remove("modal_opened");
+// }; */
+
+// closeButtons.forEach((button) => {
+//   const modal = button.closest(".modal");
+//   button.addEventListener("click", () => closeModal(modal));
+// });
